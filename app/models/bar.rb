0b90a5723b581d8a721ef99043f2c83cb4a9d847
Pierre Emmanuel Tanguy <pierre.emmanuel.tanguy@gmail.com>
2019-11-25 17:44:17 +0000

    added all the routes from all controllers in relation to bar.

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,3 +1,7 @@
 class Bar < ApplicationRecord
   belongs_to :user
+  has_many :menu_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :tables, dependent: :destroy
+
 end
