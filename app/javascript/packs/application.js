de1029cbf76c757d78325a97886fd1cdcbe51a6f
Jack Harris <jack@tallyho.cc>
2019-11-26 15:47:39 +0000

    wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,6 @@
 import "bootstrap";
-import "../dynamic_objects/basket_item_new_plus_or_minus"
+import "../dynamic_objects/basket_item_new_plus_or_minus";
+import { initMapbox } from '../plugins/init_mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+initMapbox();
