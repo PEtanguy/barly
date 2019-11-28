8bafdd9d137f104fce2c242ef4c09951848b981a
TillyFawcett <tillyfawcett@hotmail.com>
2019-11-28 14:23:51 +0000

    scrolling menu sticky nav

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,15 +2,37 @@ import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-import $ from 'jquery'
-import 'slick-carousel'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+// import $ from 'jquery'
+// import 'slick-carousel'
+// import 'slick-carousel/slick/slick.css'
+// import 'slick-carousel/slick/slick-theme.css'
 
 initMapbox();
 
-$('.drink-buttons').slick({
-  arrows: false,
-  slidesToShow: 4,
-  infinite: false
-})
+// $('.drink-buttons').slick({
+//   arrows: false,
+//   slidesToShow: 4,
+//   infinite: false
+// })
+
+
+const drinksNavbar = document.getElementById("drinks-navbar")
+
+// var sticky = drinksNavbar.offsetTop;
+
+// function myFunction() {
+const div = document.querySelector('.mhresp');
+if (div) {
+  div.addEventListener('scroll', () => {
+    if (window.scrollY >= 0.3*window.innerHeight) {
+      drinksNavbar.classList.add("sticky")
+    } else {
+      drinksNavbar.classList.remove("sticky");
+    }
+  });
+}
+// };
+
+// window.onscroll = function() {myFunction()};
+
+// myFunction()
