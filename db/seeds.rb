d03da9cf2cfd42941fd1a139417904eb7cfd7c7c
Jack Harris <jack@tallyho.cc>
2019-11-29 14:09:23 +0000

    menu item seeds for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,110 +7,110 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-MenuItem.destroy_all
+# MenuItem.destroy_all
 
-Bar.destroy_all
-User.destroy_all
-
-
-# create 1 user
-
-puts 'Creating user'
-
-1.times do
-  User.create(
-    email: 'test@barly.io',
-    password: '123456',
-    gender: 'male',
-    dob: '01/01/1871')
-end
-
-Basket.create(
-  user: User.first
-)
-
-puts 'Done creating user'
-
-puts 'Creating bars'
+# Bar.destroy_all
+# User.destroy_all
 
 
-  Bar.create(
-    name: "Jack's Pub",
-    address: 'WC2N 4HS',
-    capacity: '100',
-    opening_time: '15:00',
-    closing_time: '23:30',
-    user: User.first
-    )
-  50.times do
-  MenuItem.create(
-    name: Faker::Beer.name,
-    price: Faker::Number.decimal(l_digits: 1),
-    description: Faker::Beer.style,
-    bar: Bar.find(1),
-    category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
-    )
-  end
+# # create 1 user
+
+# puts 'Creating user'
+
+# 1.times do
+#   User.create(
+#     email: 'test@barly.io',
+#     password: '123456',
+#     gender: 'male',
+#     dob: '01/01/1871')
+# end
+
+# Basket.create(
+#   user: User.first
+# )
+
+# puts 'Done creating user'
+
+# puts 'Creating bars'
 
 
-  Bar.create(
-    name: "Tilly's Bar",
-    address: 'SE1 7RW',
-    capacity: '100',
-    opening_time: '15:00',
-    closing_time: '23:30',
-    user: User.first
-    )
-
-  50.times do
-  MenuItem.create(
-    name: Faker::Beer.name,
-    price: Faker::Number.decimal(l_digits: 1),
-    description: Faker::Beer.style,
-    bar: Bar.find(2),
-    category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
-    )
-  end
-
-
-  Bar.create(
-    name: "Rick's Speakeasy",
-    address: 'WC2N 6DU',
-    capacity: '100',
-    opening_time: '15:00',
-    closing_time: '23:30',
-    user: User.first
-    )
-  50.times do
-  MenuItem.create(
-    name: Faker::Beer.name,
-    price: Faker::Number.decimal(l_digits: 1),
-    description: Faker::Beer.style,
-    bar: Bar.find(3),
-    category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
-    )
-  end
+#   Bar.create(
+#     name: "Jack's Pub",
+#     address: 'WC2N 4HS',
+#     capacity: '100',
+#     opening_time: '15:00',
+#     closing_time: '23:30',
+#     user: User.first
+#     )
+#   50.times do
+#   MenuItem.create(
+#     name: Faker::Beer.name,
+#     price: Faker::Number.decimal(l_digits: 1),
+#     description: Faker::Beer.style,
+#     bar: Bar.find(1),
+#     category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
+#     )
+#   end
 
 
-  Bar.create(
-    name: "Pierre's Winery",
-    address: 'WC1H 0XG',
-    capacity: '100',
-    opening_time: '15:00',
-    closing_time: '23:30',
-    user: User.first
-    )
+#   Bar.create(
+#     name: "Tilly's Bar",
+#     address: 'SE1 7RW',
+#     capacity: '100',
+#     opening_time: '15:00',
+#     closing_time: '23:30',
+#     user: User.first
+#     )
 
-  Bar.create(
-    name: "Lenny's Latenight",
-    address: 'WC2E 8PS' ,
-    capacity: '100',
-    opening_time: '15:00',
-    closing_time: '23:30',
-    user: User.first
-    )
+#   50.times do
+#   MenuItem.create(
+#     name: Faker::Beer.name,
+#     price: Faker::Number.decimal(l_digits: 1),
+#     description: Faker::Beer.style,
+#     bar: Bar.find(2),
+#     category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
+#     )
+#   end
 
-  puts 'Finished creating bars'
+
+#   Bar.create(
+#     name: "Rick's Speakeasy",
+#     address: 'WC2N 6DU',
+#     capacity: '100',
+#     opening_time: '15:00',
+#     closing_time: '23:30',
+#     user: User.first
+#     )
+#   50.times do
+#   MenuItem.create(
+#     name: Faker::Beer.name,
+#     price: Faker::Number.decimal(l_digits: 1),
+#     description: Faker::Beer.style,
+#     bar: Bar.find(3),
+#     category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
+#     )
+#   end
+
+
+#   Bar.create(
+#     name: "Pierre's Winery",
+#     address: 'WC1H 0XG',
+#     capacity: '100',
+#     opening_time: '15:00',
+#     closing_time: '23:30',
+#     user: User.first
+#     )
+
+#   Bar.create(
+#     name: "Lenny's Latenight",
+#     address: 'WC2E 8PS' ,
+#     capacity: '100',
+#     opening_time: '15:00',
+#     closing_time: '23:30',
+#     user: User.first
+#     )
+
+#   puts 'Finished creating bars'
 
   puts 'Creating menu items'
 
