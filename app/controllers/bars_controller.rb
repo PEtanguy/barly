f573e8b430952e146dcdaab57125ab049b2de5f7
Jack Harris <jack@tallyho.cc>
2019-12-02 16:50:57 +0000

    added bars routes

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -62,7 +62,7 @@ class BarsController < ApplicationController
   end
 
   def my_bar
-    @bar = Bar.where(user: current_user).first
+    @bar = Bar.find(params[:id])
 
      # @bar = current_user.bar
     authorize @bar
