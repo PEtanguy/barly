e8ba74b2af71eba6ac160d5c2135775e1b424abc
Rick Thompson <rrgthompson@googlemail.com>
2019-12-03 17:13:22 +0000

    added in seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,6 +39,7 @@ puts 'Done creating user'
 puts 'Creating bars'
 
 
+
   Bar.create(
     name: "Lamb & Flag",
     address: '33 Rose St, Covent Garden, London WC2E 9EB',
@@ -50,7 +51,9 @@ puts 'Creating bars'
   50.times do
   MenuItem.create(
     name: Faker::Beer.name,
-    price: Faker::Number.decimal(l_digits: 1),
+    # price: Faker::Number.normal(mean: 5, standard_deviation: 1.5),
+    # price: rand(3.0...6.0).round(2),
+    price: [ 3.49, 3.99, 4.15, 3.75, 3.85, 6.15, 5.49, 3.95, 2.99, 3.49, 4.55, 4.75, 5.15, 5.49].sample,
     description: Faker::Beer.style,
     bar: Bar.find(1),
     category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
@@ -70,7 +73,7 @@ puts 'Creating bars'
   50.times do
   MenuItem.create(
     name: Faker::Beer.name,
-    price: Faker::Number.decimal(l_digits: 1),
+    price: [ 3.49, 3.99, 4.15, 3.75, 3.85, 6.15, 5.49, 3.95, 2.99, 3.49, 4.55, 4.75, 5.15, 5.49].sample,
     description: Faker::Beer.style,
     bar: Bar.find(2),
     category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
@@ -89,7 +92,9 @@ puts 'Creating bars'
   50.times do
   MenuItem.create(
     name: Faker::Beer.name,
-    price: Faker::Number.decimal(l_digits: 1),
+    price: [ 3.49, 3.99, 4.15, 3.75, 3.85, 6.15, 5.49, 3.95, 2.99, 3.49, 4.55, 4.75, 5.15, 5.49].sample,
+    # price: Faker::Number.decimal(l_digits: 1, range: 1..7),
+    # price: [ 3.50, 3.99, 4.20, 3.70, 3.90, 6.10, 5.40].sample,
     description: Faker::Beer.style,
     bar: Bar.find(3),
     category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
@@ -124,7 +129,8 @@ images = ["https://images.unsplash.com/photo-1504502350688-00f5d59bbdeb?ixlib=rb
   image = URI.open(images.sample)
   menu_item = MenuItem.new(
       name: Faker::Beer.name,
-      price: Faker::Number.decimal(l_digits: 1),
+      # price: rand(3.0...6.0).round(2),
+      price: [ 3.49, 3.99, 4.15, 3.75, 3.85, 6.15, 5.49, 3.95, 2.99, 3.49, 4.55, 4.75, 5.15, 5.49].sample,
       description: Faker::Beer.style,
       bar: Bar.first,
       category: ["Beer", "Wine", "Cocktail", "Mocktail", "Soft Drink"].sample
